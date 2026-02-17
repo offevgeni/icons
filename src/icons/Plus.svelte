8ff8e95fe3eee81bae6302496d87fefe8b930d63
offevgeni <solodkov-01@bk.ru>
2026-02-18 00:16:24 +0300

    add plus

---
+++ src/icons/Plus.svelte
@@ -0,0 +1,23 @@
+<script>
+  let {
+    size = 24,
+    color = 'currentColor',
+    title = '',
+    class: className = '',
+    ...rest
+  } = $props();
+</script>
+
+<svg
+  viewBox="2.4 2.4 19.2 19.2"
+  width={size}
+  height={size}
+  fill={color}
+  class="icon icon-plus{className ? ' ' + className : ''}"
+  aria-hidden={!title}
+  role={title ? "img" : "presentation"}
+  {...rest}
+>
+  {#if title}<title>{title}</title>{/if}
+  <path fill-rule="evenodd" d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1" clip-rule="evenodd"/>
+</svg>
