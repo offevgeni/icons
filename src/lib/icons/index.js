8ced8942f44d7189973366a34263d6dfab18ae73
offevgeni <solodkov-01@bk.ru>
2026-04-07 17:05:51 +0300

    chore(icons): update icon library (2026-04-07 14:05:51 UTC)

--- src/lib/icons/index.js
+++ src/lib/icons/index.js
@@ -2,3 +2,4 @@ export { default as Heart } from './Heart.svelte';
 export { default as Home } from './Home.svelte';
 export { default as Plus } from './Plus.svelte';
 export { default as Share } from './Share.svelte';
+export { default as User } from './User.svelte';
